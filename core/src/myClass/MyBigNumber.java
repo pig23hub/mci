e623bbfc2b3cbcc9ed752a39db3937e6a1b5eff0
pig23hub <heocon2701@gmail.com>
2026-09-25 23:26:18 +0700

    1. change from StringBuffer to char[] 2. add TestCase

--- core/src/myClass/MyBigNumber.java
+++ core/src/myClass/MyBigNumber.java
@@ -5,9 +5,10 @@ public class MyBigNumber {
     public MyBigNumber() {}
 
     private long sumValue(String firstNumber, String secondNumber) {
-    	StringBuffer result = new StringBuffer();
+        char[] result = new char[firstNumber.length() + 1];
         boolean tenInd = false;
         int valueFirst = 0, valueSecond = 0;
+        int resultIndex = result.length;
 
         for (int i=0; i<firstNumber.length(); i++) {
             valueFirst = Short.parseShort(firstNumber.substring(i, i+1));
@@ -23,22 +24,31 @@ public class MyBigNumber {
                 ++valueFirst;
             if (valueFirst > 9) {
                 tenInd = true;
+                valueFirst -= 10;
                 if (i < firstNumber.length()-1) {
-                    valueFirst -= 10;
                     System.out.println("Write "+valueFirst+", note 1");
-                } else
+                } else {
                     System.out.println("Write "+valueFirst);
+                }
             } else {
                 tenInd = false;
                 System.out.println("Write "+valueFirst);
             }
-			result.insert(0, valueFirst + "");
+            result[--resultIndex] = (char) (valueFirst + '0');
         }
 
-        return Long.parseLong(result.toString());
+        if (tenInd) {
+            result[--resultIndex] = '1';
+        }
+
+        long finalResult = Long.parseLong(new String(result, resultIndex, result.length - resultIndex));
+        System.out.println("Final result: "+finalResult);
+        
+        return finalResult;
     }
 
     public long sum(String stn1, String stn2) {
+    	System.out.println("\nCalc: "+stn1+" + "+stn2);
         // reverse 2 string
         stn1 = new StringBuffer(stn1).reverse().toString();
         stn2 = new StringBuffer(stn2).reverse().toString();
@@ -48,20 +58,5 @@ public class MyBigNumber {
         }
         
         return sumValue(stn2, stn1);
-    }
-
-    public static void main(String[] args) {
-        MyBigNumber myBigNumber = new MyBigNumber();
-        String stn1 = "23";
-        String stn2 = "768";
-        System.out.println("Result of sum "+stn1+" and "+stn2+" is "+myBigNumber.sum(stn1, stn2)+"\n");
-        
-        stn1 = "223";
-        stn2 = "768";
-        System.out.println("Result of sum "+stn1+" and "+stn2+" is "+myBigNumber.sum(stn1, stn2)+"\n");
-        
-        stn1 = "323";
-        stn2 = "768";
-        System.out.println("Result of sum "+stn1+" and "+stn2+" is "+myBigNumber.sum(stn1, stn2)+"\n");
     }
 }
